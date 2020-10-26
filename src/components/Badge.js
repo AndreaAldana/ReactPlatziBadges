b688efc66fe6513f7e1abfe0b089150f44d2aed1
Andrea Aldana <andrealdanapaz@gmail.com>
2020-10-26 13:57:06 -0300

    Aprendiendo props

---
+++ src/components/Badge.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from '../images/logo.svg'
+import "./styles/Badge.css"
+import avatar from '../images/Avatar.jpeg'
+
+//Los componentes son clases
+class Badge extends React.Component {
+    //Render define cual será el resultado que se verá en pantalla
+    render() {
+
+        //props, son los atributos que tienen los elementos
+
+        const { firstName, lastName, jobTitle } = this.props;
+
+        return <div className="badge">
+            <div className="badge_header"><img src={logo} alt="logo" />
+                <h2 style={{ marginTop: "12px", color: "white" }}>Proyecto React</h2>
+            </div>
+
+            <div className="badge_section-name">
+                <img className="badge_avatar" src={avatar} alt="Avatar" />
+                <h1> {firstName} <br /> {lastName}</h1></div>
+
+            <div className="badge_section-info">
+                <h3>{jobTitle}</h3>
+                <div>GitHub: <a href="https://github.com/AndreaAldana"> AndreaAldana</a>
+                </div>
+            </div>
+
+            <div className="badge_footer">#platziconf</div>
+        </div>
+    }
+}
+
+//Recordar exportar
+
+export default Badge;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Badge from './components/Badge'
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css'
+
+const element = <h1>Hello, Platzi Badges!</h1>;
+//el tipo, atributos(props), contenido
+const element1 = React.createElement('h1', {}, 'Hola! Soy un children')
+const element2 = React.createElement('a', { href: 'https://platzi.com' }, 'Ir a platzi')
+const name = 'Andrea';
+const element3 = React.createElement('h1', {}, `Hola, soy ${name}`)
+//Lo mismo pero con jsx
+const jsx = <h1>Hola, soy {name}</h1>
+const container = document.getElementById('app');
+
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(<
+    Badge firstName="Andrea"
+    lastName="Aldana"
+    jobTitle="Jr Developer" />, container);
