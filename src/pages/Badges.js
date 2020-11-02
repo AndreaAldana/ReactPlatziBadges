6ae850ffb5fcdc060ab38ebc56f555e3417f4774
Andrea Aldana <andrealdanapaz@gmail.com>
2020-11-02 17:14:02 -0300

    Probando ciclos de vida

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -4,42 +4,59 @@ import './styles/Badges.css'
 import { Link } from 'react-router-dom'
 
 class Badges extends React.Component {
-    state = {
-        data: [{
-            id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-            firstName: "Anubis",
-            lastName: "Ali",
-            email: "anubis_ali@gmail.com",
-            jobTitle: "Senior backend developer",
-            gitHub: "Thracium",
-            avatarUrl:
-                "https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png"
-        },
-        {
-            id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-            firstName: "Paloma",
-            lastName: "Herrera",
-            email: "pherrera@hotmail.com",
-            jobTitle: "Ui/ux designer",
-            gitHub: "PalomaHerrera",
-            avatarUrl:
-                "https://clevelandschoolsbookfund.org/wp-content/uploads/2019/01/icon-profile-female.png"
-        },
-        {
-            id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-            firstName: "Catalina",
-            lastName: "Contreras",
-            email: "cata94_@hotmail.com",
-            jobTitle: "QA Analyst",
-            gitHub: "CataDesigns",
-            avatarUrl:
-                "https://www.clementjuniorpreschool.com/wp-content/uploads/2019/04/219969.png"
-        }
 
-        ]
-    };
+
+    constructor(props) {
+        super(props);
+        console.log('1, constructor');
+        this.state = {
+            data: []
+        };
+    }
+
+    componentDidMount() {
+        console.log('3, componentDidMount()')
+
+        setTimeout(() => {
+            this.setState({
+                data: [{
+                    id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
+                    firstName: "Anubis",
+                    lastName: "Ali",
+                    email: "anubis_ali@gmail.com",
+                    jobTitle: "Senior backend developer",
+                    gitHub: "Thracium",
+                    avatarUrl:
+                        "https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png"
+                },
+                {
+                    id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
+                    firstName: "Paloma",
+                    lastName: "Herrera",
+                    email: "pherrera@hotmail.com",
+                    jobTitle: "Ui/ux designer",
+                    gitHub: "PalomaHerrera",
+                    avatarUrl:
+                        "https://clevelandschoolsbookfund.org/wp-content/uploads/2019/01/icon-profile-female.png"
+                },
+                {
+                    id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
+                    firstName: "Catalina",
+                    lastName: "Contreras",
+                    email: "cata94_@hotmail.com",
+                    jobTitle: "QA Analyst",
+                    gitHub: "CataDesigns",
+                    avatarUrl:
+                        "https://www.clementjuniorpreschool.com/wp-content/uploads/2019/04/219969.png"
+                }
+
+                ]
+            })
+        }, 3000)
+    }
 
     render() {
+        console.log('2, render ')
         return <div>
             <div className="Badges__container">
                 <div className="Badges__buttons">
