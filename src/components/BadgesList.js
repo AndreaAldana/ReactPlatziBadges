736ada3029abf993a44b5ef86aa754c4dd396ccb
Andrea Aldana <andrealdanapaz@gmail.com>
2020-11-04 19:40:58 -0300

    Filtro de badges sirviendo con Hooks

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -3,64 +3,95 @@ import './styles/BadgeList.css'
 import { Link } from 'react-router-dom'
 import Gravatar from "./Gravatar";
 
-class BadgesList extends React.Component {
-    render() {
-        if (this.props.badges.length == 0) {
-            return <div className="text-center" style={{ height: "20pc" }}>
-                <div>
-                    <h3>No badges were found</h3>
-                    <Link className="btn btn mb-4" style={{ backgroundColor: "#F4976C", color: "white" }} to="/badges/new"> Create new badge</Link>
+function BadgesList(props) {
 
+    const [query, setQuery] = React.useState('')
+
+
+    const badges = props.badges;
+
+    const [filteredBadges, setFilteredBadges] = React.useState(badges)
+
+    React.useMemo(
+        () => {
+            const result = badges.filter(badge => {
+                return `${badge.firstName} ${badge.lastName}`
+                    .toLowerCase()
+                    .includes(query.toLowerCase());
+            })
+
+            setFilteredBadges(result)
+        }, [badges, query]);
+
+
+    if (filteredBadges.length == 0) {
+        return <div className="text-center" style={{ height: "20pc" }}>
+            <div>
+                <div className="form-group">
+                    <label>Filter badges</label>
+                    <input value={query} onChange={(e) => { setQuery(e.target.value) }} type="text" className="form-control" />
                 </div>
+                <h3>No badges were found</h3>
+                <Link className="btn btn mb-4" style={{ backgroundColor: "#F4976C", color: "white" }} to="/badges/new"> Create new badge</Link>
+
             </div>
-        }
-        const badgesList = [...this.props.badges].reverse();
-        return <ul className="list-unstyled" >
-            {/*   se recorre el arreglo */}
-
-            {badgesList.map((badge) => {
-
-                return (
-                    //Esto es para que las reconozca por id y tengan su key
-                    <div className="whole-element">
-
-
-                        <li key={badge.id} className="Badge__card">
-                            <Gravatar email={badge.email} alt="" className="BadgesListItem__avatar" />
-                            <div className="Badges__letters">
-
-                                <div>
-                                    <div className="user__icon"></div>
-                                    <strong>{badge.firstName} {badge.lastName}</strong>
-                                </div>
-                                <br />
-                                <div>
-                                    <div className="git__icon"></div>
-                                    <span>{badge.twitter}</span>
-
-                                </div>
-                                <br />
-                                <div>
-                                    <div className="job__icon"></div>
-                                    <span>{badge.jobTitle}</span>
-
-                                </div>
-                                <div style={{ paddingTop: "2em" }}>
-                                    <Link
-                                        className="btn btn mr-1"
-                                        style={{ backgroundColor: "#F4976C", color: "white" }}
-                                        to={`/badges/${badge.id}`}>
-                                        Details
-                        </Link>
-                                </div>
-                            </div>
-                        </li>
-                    </div>
-
-                )
-            })}
-        </ul>
+        </div>
     }
+
+    const badgesList = badges.reverse();
+    return <ul className="list-unstyled" >
+        <div className="form-group">
+            <label>Filter badges</label>
+            <input value={query}
+                onChange={(e) => { setQuery(e.target.value) }}
+                type="text"
+                className="form-control" />
+        </div>
+        {/*   se recorre el arreglo */}
+
+
+        {filteredBadges.map((badge) => {
+
+            return (
+                //Esto es para que las reconozca por id y tengan su key
+
+                <div className="whole-element">
+
+                    <li key={badge.id} className="Badge__card">
+                        <Gravatar email={badge.email} alt="" className="BadgesListItem__avatar" />
+                        <div className="Badges__letters">
+
+                            <div>
+                                <div className="user__icon"></div>
+                                <strong>{badge.firstName} {badge.lastName}</strong>
+                            </div>
+                            <br />
+                            <div>
+                                <div className="git__icon"></div>
+                                <span>{badge.twitter}</span>
+
+                            </div>
+                            <br />
+                            <div>
+                                <div className="job__icon"></div>
+                                <span>{badge.jobTitle}</span>
+
+                            </div>
+                            <div style={{ paddingTop: "2em" }}>
+                                <Link
+                                    className="btn btn mr-1"
+                                    style={{ backgroundColor: "#F4976C", color: "white" }}
+                                    to={`/badges/${badge.id}`}>
+                                    Details
+                        </Link>
+                            </div>
+                        </div>
+                    </li>
+                </div>
+
+            )
+        })}
+    </ul>
 }
 
 export default BadgesList
